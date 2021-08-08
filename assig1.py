1f5b758e4a1008ffb4f19510cda712b2850ab509
Sharmila1405 <88608145+Sharmila1405@users.noreply.github.com>
2021-08-08 18:52:33 +0530

    Update assig1.py

--- assig1.py
+++ assig1.py
@@ -1,11 +1,10 @@
-
-
+# 1st one
 import math
 radius = float(input("Input the radius of a circle: "));
 area = math.pi*radius**2;
 print("The area of the circle with radius {} is: {}".format(radius,area));
 
-
+#2nd one
 
 dict = {"py":"python","java":"Java","c":"C","hmt":"Html"} 
 filename = input("Input the Filename: ");
